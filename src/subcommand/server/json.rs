11d203d36b5fd122b10f3e69774ac4a5e90d791c
Benjamin DeMann <benjamin.demann@gmail.com>
2023-03-01 07:32:09 -0700

    endpoints by id

--- src/subcommand/server/json.rs
+++ src/subcommand/server/json.rs
@@ -1,5 +1,5 @@
 use axum::{extract::Path, Extension};
-use bitcoin::{Address, BlockHash, OutPoint, Script, Transaction, TxOut, Txid};
+use bitcoin::{Address, Block, BlockHash, OutPoint, Script, Transaction, TxOut, Txid};
 use serde::{Deserialize, Serialize};
 use serde_json::Error;
 use std::sync::Arc;
@@ -72,6 +72,25 @@ impl Server {
     Ok(handle_json_result(serde_json::to_string(&block_json)))
   }
 
+  pub(super) async fn latest_block_id(
+    Extension(index): Extension<Arc<Index>>,
+  ) -> ServerResult<String> {
+    let block_hash = index
+      .blocks(1)?
+      .iter()
+      .map(|(_, block_hash)| block_hash)
+      .collect::<Vec<_>>()[0]
+      .clone();
+
+    let block_info = index
+      .block_header_info(block_hash)?
+      .ok_or_not_found(|| format!("block {block_hash}"))?;
+
+    Ok(handle_json_result(serde_json::to_string(
+      &block_info.height,
+    )))
+  }
+
   pub(super) async fn inscription_json_by_id(
     Extension(page_config): Extension<Arc<PageConfig>>,
     Extension(index): Extension<Arc<Index>>,
@@ -97,7 +116,7 @@ impl Server {
     )
   }
 
-  pub(super) async fn outputs_for_block(
+  pub(super) async fn outputs_for_block_by_hash(
     Extension(page_config): Extension<Arc<PageConfig>>,
     Extension(index): Extension<Arc<Index>>,
     Path(DeserializeFromStr(block_hash)): Path<DeserializeFromStr<BlockHash>>,
@@ -108,36 +127,23 @@ impl Server {
       None => return Ok("[]".to_string()),
     };
 
-    let transactions = block.txdata;
+    let outputs = get_outputs_from_block(&block, &index, &page_config);
 
-    let outputs: ServerResult<Vec<OutputJson>> =
-      transactions.iter().fold(Ok(vec![]), |acc, transaction| {
-        let mut acc = acc?;
-        let output_results: ServerResult<Vec<OutputJson>> = transaction
-          .output
-          .iter()
-          .enumerate()
-          .fold(Ok(vec![]), |acc, (vout, output)| {
-            let mut acc = acc?;
-            let outpoint = OutPoint::new(transaction.txid(), vout as u32);
-            let inscriptions = index.get_inscriptions_on_output(outpoint)?;
-            if inscriptions.len() > 0 {
-              acc.push(OutputJson {
-                inscriptions,
-                value: output.value,
-                script_pubkey: output.script_pubkey.clone(),
-                address: page_config
-                  .chain
-                  .address_from_script(&output.script_pubkey)
-                  .ok(),
-                transaction: transaction.txid(),
-              });
-            }
-            Ok(acc)
-          });
-        acc.extend(output_results?);
-        Ok(acc)
-      });
+    Ok(handle_json_result(serde_json::to_string(&outputs?)))
+  }
+
+  pub(super) async fn outputs_for_block(
+    Extension(page_config): Extension<Arc<PageConfig>>,
+    Extension(index): Extension<Arc<Index>>,
+    Path(DeserializeFromStr(block_index)): Path<DeserializeFromStr<u64>>,
+  ) -> ServerResult<String> {
+    let block_option = index.get_block_by_height(block_index)?;
+    let block = match block_option {
+      Some(block) => block,
+      None => return Ok("[]".to_string()),
+    };
+
+    let outputs = get_outputs_from_block(&block, &index, &page_config);
 
     Ok(handle_json_result(serde_json::to_string(&outputs?)))
   }
@@ -145,6 +151,22 @@ impl Server {
   pub(super) async fn inscriptions_for_block(
     Extension(page_config): Extension<Arc<PageConfig>>,
     Extension(index): Extension<Arc<Index>>,
+    Path(DeserializeFromStr(block_index)): Path<DeserializeFromStr<u64>>,
+  ) -> ServerResult<String> {
+    let block_option = index.get_block_by_height(block_index)?;
+    let block = match block_option {
+      Some(block) => block,
+      None => return Ok("[]".to_string()),
+    };
+
+    let inscriptions = get_inscriptions_from_block(&block, &index, &page_config);
+
+    Ok(handle_json_result(serde_json::to_string(&inscriptions?)))
+  }
+
+  pub(super) async fn inscriptions_for_block_by_hash(
+    Extension(page_config): Extension<Arc<PageConfig>>,
+    Extension(index): Extension<Arc<Index>>,
     Path(DeserializeFromStr(block_hash)): Path<DeserializeFromStr<BlockHash>>,
   ) -> ServerResult<String> {
     let block_option = index.get_block_by_hash(block_hash)?;
@@ -153,30 +175,7 @@ impl Server {
       None => return Ok("[]".to_string()),
     };
 
-    let transactions = block.txdata;
-
-    let inscriptions: ServerResult<Vec<InscriptionJson>> =
-      transactions.iter().fold(Ok(vec![]), |acc, transaction| {
-        let mut acc = acc?;
-        let output_results: ServerResult<Vec<InscriptionJson>> = transaction
-          .output
-          .iter()
-          .enumerate()
-          .fold(Ok(vec![]), |acc, (vout, _)| {
-            let mut acc = acc?;
-            let outpoint = OutPoint::new(transaction.txid(), vout as u32);
-            let inscriptions_ids = index.get_inscriptions_on_output(outpoint)?;
-            let inscriptions: ServerResult<Vec<InscriptionJson>> = inscriptions_ids.iter().fold(Ok(vec![]), |acc, inscription_id| {
-              let mut acc = acc?;
-              acc.push(build_inscription(inscription_id, &index, &page_config)?);
-              Ok(acc)
-            });
-            acc.extend(inscriptions?);
-            Ok(acc)
-          });
-        acc.extend(output_results?);
-        Ok(acc)
-      });
+    let inscriptions = get_inscriptions_from_block(&block, &index, &page_config);
 
     Ok(handle_json_result(serde_json::to_string(&inscriptions?)))
   }
@@ -297,4 +296,74 @@ fn handle_json_result(result: Result<String, Error>) -> String {
     Ok(json) => json,
     Err(err) => err.to_string(),
   }
+}
+
+fn get_outputs_from_block(
+  block: &Block,
+  index: &Arc<Index>,
+  page_config: &Arc<PageConfig>,
+) -> ServerResult<Vec<OutputJson>> {
+  let transactions = &block.txdata;
+
+  transactions.iter().fold(Ok(vec![]), |acc, transaction| {
+    let mut acc = acc?;
+    let output_results: ServerResult<Vec<OutputJson>> =
+      transaction
+        .output
+        .iter()
+        .enumerate()
+        .fold(Ok(vec![]), |acc, (vout, output)| {
+          let mut acc = acc?;
+          let outpoint = OutPoint::new(transaction.txid(), vout as u32);
+          let inscriptions = index.get_inscriptions_on_output(outpoint)?;
+          if inscriptions.len() > 0 {
+            acc.push(OutputJson {
+              inscriptions,
+              value: output.value,
+              script_pubkey: output.script_pubkey.clone(),
+              address: page_config
+                .chain
+                .address_from_script(&output.script_pubkey)
+                .ok(),
+              transaction: transaction.txid(),
+            });
+          }
+          Ok(acc)
+        });
+    acc.extend(output_results?);
+    Ok(acc)
+  })
+}
+
+fn get_inscriptions_from_block(
+  block: &Block,
+  index: &Arc<Index>,
+  page_config: &Arc<PageConfig>,
+) -> ServerResult<Vec<InscriptionJson>> {
+  let transactions = &block.txdata;
+
+  transactions.iter().fold(Ok(vec![]), |acc, transaction| {
+    let mut acc = acc?;
+    let output_results: ServerResult<Vec<InscriptionJson>> = transaction
+      .output
+      .iter()
+      .enumerate()
+      .fold(Ok(vec![]), |acc, (vout, _)| {
+        let mut acc = acc?;
+        let outpoint = OutPoint::new(transaction.txid(), vout as u32);
+        let inscriptions_ids = index.get_inscriptions_on_output(outpoint)?;
+        let inscriptions: ServerResult<Vec<InscriptionJson>> =
+          inscriptions_ids
+            .iter()
+            .fold(Ok(vec![]), |acc, inscription_id| {
+              let mut acc = acc?;
+              acc.push(build_inscription(inscription_id, &index, &page_config)?);
+              Ok(acc)
+            });
+        acc.extend(inscriptions?);
+        Ok(acc)
+      });
+    acc.extend(output_results?);
+    Ok(acc)
+  })
 }
